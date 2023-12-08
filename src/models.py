d692169e7f6354dbaf3509521f0e2097d61768bf
Empitrix <empitrix@gmail.com>
2023-12-08 22:36:11 +0330

    Added: word_api, api model, and mux them

--- src/models.py
+++ src/models.py
@@ -1,8 +1,90 @@
-class WordResponseAPI:
-	def __init__(self, word:str, pronunciation:str,
-			  definition:str, examples:list[str]) -> None:
-		self.word = word
-		self.pronunciation = pronunciation
+class LicenseAPI:
+	def __init__(self, name:str, url:str) -> None:
+		self.name = name
+		self.url = url
+
+
+class Definition:
+	def __init__(self,
+			example:str, definition:str,
+			synonyms:list[str], antonyms:list[str]) -> None:
 		self.definition = definition
-		self.examples = examples
+		self.synonyms = synonyms
+		self.antonyms = antonyms
+		self.example = example
+
+
+class Pronunciation:
+	def __init__(self, text:str, audio:str) -> None:
+		self.text = text
+		self.audio = audio 
+
+
+class Phonetic:
+	def __init__(self, audio:str, source_url:str, file_license:LicenseAPI) -> None:
+		self.audio = audio
+		self.source_url = source_url
+		self.file_license = file_license
+
+
+class WordResponseAPI:
+	def __init__(self, word:str, pronunciation:Pronunciation, phonetics:list[Phonetic],
+			  part_of_speech:str, source_urls:list[str], file_license:LicenseAPI,
+			  synonyms:list[str], antonyms:list[str],
+			  definitions:list[Definition]) -> None:
+		self.word = word
+		self.pronunciation = pronunciation,
+		self.phonetics = phonetics
+		self.definitions = definitions
+		self.part_of_speech = part_of_speech
+		self.source_urls = source_urls
+		self.file_license = file_license
+		self.synonyms = synonyms
+		self.antonyms = antonyms
+
+
+def word_response_parser(inpt:dict) -> WordResponseAPI:
+	"""Convert JSON file into WordResponseAPI"""
+	definitions:list[Definition] = []
+	pronunciation:Pronunciation = Pronunciation(text="", audio="")
+	phonetics:list[Phonetic] = []
+
+	for df in inpt["meanings"][0]["definitions"]:
+		print(df)
+		definitions.append(Definition(
+			definition = df["definition"],
+			synonyms = df["synonyms"],
+			antonyms = df["antonyms"],
+			example = df["example"] if "example" in list(df.keys()) else "",
+		))
+	for p in inpt["phonetics"]:
+		if len(list(p.keys())) == 2:
+			if pronunciation.text == "":
+				pronunciation = Pronunciation(
+					text=p["text"],
+					audio=p["audio"])
+			else: continue
+		elif len(list(p.keys())) == 3:
+			phonetics.append(Phonetic(
+				audio=p["audio"],
+				source_url=p["sourceUrl"],
+				file_license = LicenseAPI(
+					name=p["license"]["name"],
+					url=p["license"]["url"])
+			))
+		else: continue
+
+	return WordResponseAPI(
+		word=inpt["word"],
+		pronunciation=pronunciation,
+		phonetics=phonetics,
+		definitions=definitions,
+		part_of_speech=inpt["meanings"][0]["partOfSpeech"],
+		synonyms=inpt["meanings"][0]["synonyms"],
+		antonyms=inpt["meanings"][0]["antonyms"],
+		source_urls=inpt["sourceUrls"],
+		file_license=LicenseAPI(
+			name=inpt["license"]["name"],
+			url=inpt["license"]["url"]),  # License
+	)
 
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,28 @@
+import sys
 from requests import get
+from models import WordResponseAPI, word_response_parser
+from visual import prt
+from json import loads
 
-def get_api_response(word:str) -> dict:
+
+def get_api_response(word:str) -> WordResponseAPI:
 	"""Get the input word data from server"""
-	response = get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word.strip()}')
-	if(response.status_code == 200):
-		return response.json();
-	return {}
+	word = word.strip().lower()  # Convert word to lowercase
+	try:
+		response = get(f'https://api.dictionaryapi.dev/api/v2/entries/en/{word.strip()}')
+		if(response.status_code == 200):
+			data = loads(response.text)[0]
+			return word_response_parser(data)
+		else:
+			prt("[red]Error on API")
+			sys.exit()
+	except(Exception,) as error:
+		prt("[red]Something is Wrong, try again later!")
+		prt(f"[red]ERROR: [yellow]{error}")
+		sys.exit()
+
+
+if __name__ == "__main__":
+	word:WordResponseAPI = get_api_response("Define")
+	print(word.word)
+
